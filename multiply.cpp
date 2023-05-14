578439ec5e72daa660d122be00e18b82224755d0
Aabha Soni <aabhasoni500@gmail.com>
2023-05-14 19:55:21 +0530

    adding into.cpp

---
+++ multiply.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    int mul=a*b;
+    cout<<"Multiplication is "<<mul<<endl;
+
+    return 0;
+}
