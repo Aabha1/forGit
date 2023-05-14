d066861cf250ab5c49d255cdd06ea6935cb5b7cd
Aabha Soni <aabhasoni500@gmail.com>
2023-05-14 19:18:40 +0530

    initial

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+
+    cout<<"enter a : ";
+    cin>>a;
+    cout<<"enter b : ";
+    cin>>b;
+    int sum=a+b;
+    cout<<"Sum is "<<sum<<endl;
+
+    return 0;
+}
